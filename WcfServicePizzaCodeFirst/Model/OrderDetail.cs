a2d979cd6a5a2f575e1d401132a17923bf87eac0
Muhammad Rehan Qadri <muhammadrehanqadri@outlook.com>
2015-07-10 17:06:48 +0500

    T

--- WcfServicePizzaCodeFirst/Model/OrderDetail.cs
+++ WcfServicePizzaCodeFirst/Model/OrderDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,7 @@ namespace WcfServicePizzaCodeFirst.Model
 {
     public class OrderDetail
     {
-
+        [Key]
         public int OrderId { get; set; }
         public Order Order { get; set; }
         public string PizzaName { get; set; }
